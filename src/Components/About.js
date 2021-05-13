633db29147c6463346946284e473ccc79fdd5e0f
Som-Jinghania <arksec25@gmail.com>
2021-05-13 21:35:34 +0530

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            Welocome to about page
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto error corporis excepturi a enim. Illum cum architecto, esse cupiditate possimus ad expedita? Quisquam voluptatem quibusdam natus voluptate soluta eum deserunt?</p>
+        </div>
+    )
+}
